ed2b8a736d5c95a3a360d8a716cc242c79f4e13f
bamidele100 <bamideletalabi01@gmail.com>
2023-04-01 19:37:44 -0300

    Update on public images folder, component folder, pages folder, styles folder, App.js and index.js

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -3,12 +3,13 @@ import React from "react";
 function Portfolio() {
   return (
     <div className="portfolio">
+  
       <div className="image">
-      <img className="generator" src="/public/images/JavaScript-Password-Generator.jpg" alt="" />
-        <img className="tracker" src="/public/images/Employee-Tracker.png" alt="" />
-        <img className="member" src="/public/images/My-Team-Member.png" alt="" />
-        <img className="note" src="/public/images/Note-Title.png" alt="" />
-        <img className="readme" src="/public/images/Professional-Readme-Generator.png" alt="" />
+      <img className="tracker" src={process.env.PUBLIC_URL + "/images/Employee.png"} width= "400" alt="Employee-Tracker"/>
+      <img className="generator" src={process.env.PUBLIC_URL + "/images/JavaScript-Password-Generator.jpg"} width= "400" alt="JavaScript-Password-Generator"/>
+      <img className="member" src={process.env.PUBLIC_URL + "/images/My-Team-Member.png"} width= "400" alt="My-Team-Member"/>
+      <img className="note" src={process.env.PUBLIC_URL + "/images/Note-Title.png"} width= "400" alt="Note-Title"/>
+      <img className="readme" src={process.env.PUBLIC_URL + "/images/Professional-Readme-Generator.png"} width= "400" alt="Professional-Readme-Generator"/>
       </div>
     </div>
   );
