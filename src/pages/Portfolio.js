42a45d013c498dc8ea526ea44f6cacb4c3ffc4a6
bamidele100 <bamideletalabi01@gmail.com>
2023-04-01 17:19:17 -0300

    edit on public code

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -4,11 +4,11 @@ function Portfolio() {
   return (
     <div className="portfolio">
       <div className="image">
-      <img className="generator" src="images/JavaScript-Password-Generator.jpg" alt="" />
-        <img className="tracker" src="images/Employee-Tracker.png" alt="" />
-        <img className="member" src="images/My-Team-Member.png" alt="" />
-        <img className="note" src="images/Note-Title.png" alt="" />
-        <img className="readme" src="images/Professional-Readme-Generator.png" alt="" />
+      <img className="generator" src="/public/images/JavaScript-Password-Generator.jpg" alt="" />
+        <img className="tracker" src="/public/images/Employee-Tracker.png" alt="" />
+        <img className="member" src="/public/images/My-Team-Member.png" alt="" />
+        <img className="note" src="/public/images/Note-Title.png" alt="" />
+        <img className="readme" src="/public/images/Professional-Readme-Generator.png" alt="" />
       </div>
     </div>
   );
