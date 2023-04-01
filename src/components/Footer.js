3aabb4d4fdf7015d96a564742962e66214be848a
bamidele100 <bamideletalabi01@gmail.com>
2023-04-01 17:24:49 -0300

    edit on component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ function footer() {
     <div className='footer'>
       <div className='socialMedia'>
       <SocialIcon className='linkin' url="https://linkedin.com/in/bamideletalabi" /> 
-      <SocialIcon url="https://github.com/Bamidele100/web-api-code-quiz.git" />
+      <SocialIcon url="https://github.com/Bamidele100/react-portfolio.git" />
       </div>
       <p>&copy; 2023 Bamidele</p>
     </div>
